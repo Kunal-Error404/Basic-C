811e124b60b18a6d67ceebc973c0139d3a67ffeb
Kunal-Error404 <kunalsaini73112@gmail.com>
2025-08-02 21:31:05 +0530

    NEW WAPS

---
+++ Cubeofanumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float a;
+    printf("Enter the side of the cube:/n");
+    scanf("%f",&a);
+    float cube = a*a*a;
+    printf("Cube of the number is: %f",cube);
+    return 0;
+    
+}
