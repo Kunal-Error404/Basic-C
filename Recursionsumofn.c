5872cea4988595d0eff6d0429d6e78bfcdb54cb7
Kunal-Error404 <kunalsaini73112@gmail.com>
2025-08-09 13:24:12 +0530

    Recursion

--- Recursionsumofn.c
+++ Recursionsumofn.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int sum(int n);
 int main(){
-    printf("sum of n natural numbers is %d",sumn());
+    printf("sum of n natural numbers is %d",sumn(5));
     return 0;
 }
 int sum(int n){
